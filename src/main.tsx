743e97dd38107ea68e55a3f3d28b6e970c1a968d
byzh <boying.zhu1106@gmail.com>
2026-02-06 04:05:09 +0100

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; // 关键：必须引入这个文件
-import App from './App';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
